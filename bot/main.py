34fc003a8485eed167ad87345da3b896570ffa1f
anekobtw <krolexonder@gmail.com>
2024-05-29 09:33:58 +0400

    for the hosting

--- bot/main.py
+++ bot/main.py
@@ -5,6 +5,7 @@ import os
 from aiogram import Bot, Dispatcher
 from aiogram.client.bot import DefaultBotProperties
 from aiogram.fsm.storage.memory import MemoryStorage
+from aiogram.client.session.aiohttp import AiohttpSession
 from dotenv import load_dotenv
 
 from handlers.common import router
@@ -13,6 +14,8 @@ from handlers.common import router
 async def run_bot():
     load_dotenv()
     TOKEN = os.getenv("TOKEN")
+    # session = AiohttpSession(proxy="http://proxy.server:3128")
+    # bot = Bot(token=TOKEN, default=DefaultBotProperties(parse_mode="HTML"), session=session)
     bot = Bot(token=TOKEN, default=DefaultBotProperties(parse_mode="HTML"))
 
     logging.basicConfig(
